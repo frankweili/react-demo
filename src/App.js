6fa8786f56d4696bc838b89026d85a1962ea6bf7
frankwei <1183806196@qq.com>
2020-10-19 21:35:01 +0800

    shouldComponentUpdate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+class App extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            n:1
+        }
+    }
+    onClick = () => {
+        this.setState(state => { n: this.state.n += 1 })
+        this.setState(state=>{n:this.state.n-=1})
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextState === this.state) {
+            return false
+        } else {
+            return true
+        }
+    }
+    render() {
+        console.log('更新')
+        return (
+            <div>
+                <div> App 新世界</div>
+                {this.state.n}
+                <button onClick={this.onClick}>+1</button>
+            </div>
+        )
+    }
+    
 }
-
-export default App;
+export default App
